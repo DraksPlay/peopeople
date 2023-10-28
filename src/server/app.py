83d0851c6b9e3ec89886fde2b73684a33110fee1
DraksPlay <draksplay@mail.ru>
2023-10-28 21:53:43 +0300

    run_server create

--- src/server/app.py
+++ src/server/app.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 from src.config import API_URL
 from api.users.router import router as users_router
 
-app = FastAPI(docs_url=API_URL, title="PeoPeoPle API")
+app = FastAPI(docs_url=API_URL, openapi_url="")
 
-app.include_router(users_router, prefix=API_URL, tags=["User"])
+app.include_router(users_router, prefix=API_URL, tags=["User"], include_in_schema=False)
